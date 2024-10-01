e862176914e5b77d0b4f9f64f37562efddfe1eec
Rahul Tripathi <tripathirahul796@gmail.com>
2024-10-01 22:35:39 +0530

    "first commit"

---
+++ src/main/kotlin/com/rahul/ordersystem/OrdersystemApplication.kt
@@ -0,0 +1,11 @@
+package com.rahul.ordersystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrdersystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrdersystemApplication>(*args)
+}
